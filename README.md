e252e0747cbcd685979e7ba6bd8fb1ec3960373a
qingxp9 <qingxp9@gmail.com>
2018-01-22 12:29:49 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # WiFi-Miner-Detector
+by qingxp9 @ 360PegasusTeam
+
+##Overview
 Detecting malicious WiFi with mining cryptocurrency.
+
+##Requirements
+```
+sudo apt install python-pip
+pip install scapy
+pip install scapy_http
+```
+
+And you'll need a WiFi card that supports monitor mode. You can check by running: iw list. Something like:
+
+```
+	Supported interface modes:
+		 * IBSS
+		 * managed
+		 * AP
+		 * AP/VLAN
+		 * monitor
+		 * mesh point
+```
+
+I test on TP-Link TL-WN722N (chipset Atheros AR9271), and it works well.
+
+##Usage
+sudo python wifi_miner_detector.py wlan0
+
+![demo](https://github.com/360PegasusTeam/WiFi-Miner-Detector/blob/master/demo.gif)
---
+++ wifi_miner_detector.py
@@ -0,0 +1,49 @@
+from scapy.all import *
+from scapy.layers import http
+import os
+
+if len(sys.argv) == 2:
+  iface = str(sys.argv[1])
+else:
+  iface = "wlx4494fc2560a1"
+
+filter_response = "tcp src port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"
+channels = [1,6,11]
+ap_dict = {}
+
+def BeaconHandler(pkt) :
+  if pkt.haslayer(Dot11) :
+    if pkt.type == 0 and pkt.subtype == 8 :
+        if pkt.addr2 not in ap_dict.keys() :
+            ap_dict[pkt.addr2] = pkt.info
+
+def HTTPHandler(pkt):
+  if pkt.haslayer('HTTP'):
+    if "CoinHive.Anonymous" in pkt.load:
+      mac = pkt.addr2
+      if mac in ap_dict.keys() :
+          ssid = ap_dict[mac]
+          reason = "Coinhive_miner"
+          print "Find Rogue AP: %s(%s) -- %s" %(ssid, mac, reason)
+      else:
+          print mac
+
+print "WiFi-Miner-Detector"
+print "Detecting malicious WiFi with mining cryptocurrency.\n"
+print "https://github.com/360PegasusTeam/WiFi-Miner-Detector"
+print "by qingxp9 @ 360PegasusTeam\n"
+
+print "[+] Set iface %s to monitor mode" %(iface)
+os.system("ifconfig " + iface + " down")
+os.system("iwconfig " + iface + " mode monitor")
+os.system("ifconfig " + iface + " up")
+
+print "[+] Sniffing on channel " + str(channels)
+while True:
+    for channel in channels:
+        os.system("iwconfig " + iface + " channel " + str(channel))
+        #print "[+] Sniffing on channel " + str(channel)
+#Get surrounding WiFi SSID
+        sniff(iface=iface, prn=BeaconHandler, timeout=1)
+#Analyze HTTP
+        sniff(iface=iface, prn=HTTPHandler, filter=filter_response, timeout=10)
