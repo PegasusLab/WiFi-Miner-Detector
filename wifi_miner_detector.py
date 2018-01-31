b5e0f0dab165cf2d83d1e5f6b7adbf01efad0ca6
qingxp9 <qingxp9@gmail.com>
2018-01-31 19:58:25 +0800

    update beacon filter

--- wifi_miner_detector.py
+++ wifi_miner_detector.py
@@ -8,25 +8,25 @@ else:
   iface = "wlx4494fc2560a1"
 
 filter_response = "tcp src port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"
+filter_beacon = "subtype probe-resp or subtype beacon"
+
 channels = [1,6,11]
 ap_dict = {}
 
 def BeaconHandler(pkt) :
-  if pkt.haslayer(Dot11) :
-    if pkt.type == 0 and pkt.subtype == 8 :
-        if pkt.addr2 not in ap_dict.keys() :
-            ap_dict[pkt.addr2] = pkt.info
+  if pkt.addr2 not in ap_dict.keys() :
+    ap_dict[pkt.addr2] = pkt.info
 
 def HTTPHandler(pkt):
   if pkt.haslayer('HTTP'):
     if "CoinHive.Anonymous" in pkt.load:
       mac = pkt.addr2
       if mac in ap_dict.keys() :
-          ssid = ap_dict[mac]
-          reason = "Coinhive_miner"
-          print "Find Rogue AP: %s(%s) -- %s" %(ssid, mac, reason)
+        ssid = ap_dict[mac]
+        reason = "Coinhive_miner"
+        print "Find Rogue AP: %s(%s) -- %s" %(ssid, mac, reason)
       else:
-          print mac
+        print mac
 
 print "WiFi-Miner-Detector"
 print "Detecting malicious WiFi with mining cryptocurrency.\n"
@@ -44,6 +44,6 @@ while True:
         os.system("iwconfig " + iface + " channel " + str(channel))
         #print "[+] Sniffing on channel " + str(channel)
 #Get surrounding WiFi SSID
-        sniff(iface=iface, prn=BeaconHandler, timeout=1)
+        sniff(iface=iface, prn=BeaconHandler, filter=filter_beacon , timeout=1)
 #Analyze HTTP
         sniff(iface=iface, prn=HTTPHandler, filter=filter_response, timeout=10)
